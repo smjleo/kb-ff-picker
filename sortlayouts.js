e4daba7c8cafe93290e04f405ea5111b7971a5fa
smjleo <leolsj1023@gmail.com>
2019-01-29 07:59:33 +0800

    Completed sorting function

--- sortlayouts.js
+++ sortlayouts.js
@@ -4,9 +4,9 @@ key
 esc: escape
 alpha: a-z + shift + enter + backspace + tab
 topnum: 1-0
-l/r_ctrl: controls
-l/r_alt: alts
-l/r_os: command (mac), windows (windows)
+(l|r)_ctrl: controls
+(l|r)_alt: alts
+(l|r)_os: command (mac), windows (windows)
 arrow: arrows
 function: F1-F12
 function2: buttons above the arrows in a fullsized/tkl keyboard (insert, page down, etc)
@@ -15,19 +15,43 @@ numpad: number pads on the very right side of fullsized keyboard
 
 function sort_ff(desired){
   let formfactors = [
-    ["Numpad", ["numpad"]],
-    ["40%", ["esc", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt"]],
-    ["60%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt"]],
-    ["60% - HHKB", ["esc", "topnum", "alpha", "l_ctrl", "l_os", "r_os", "l_alt", "r_alt"]],
-    ["60% - Winkeyless", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_alt", "r_alt"]],
-    ["64%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow"]],
-    ["68%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2"]],
-    ["75%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function"]],
-    ["Tenkeyless", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function"]],
-    ["1800", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function", "numpad"]],
-    ["Fullsized", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function", "numpad"]]
+    ["Numpad", ["numpad"], 0],
+    ["40%", ["esc", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt"], 0],
+    ["60%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt"], 0],
+    ["60% - HHKB", ["esc", "topnum", "alpha", "l_ctrl", "l_os", "r_os", "l_alt", "r_alt"], 0],
+    ["60% - Winkeyless", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_alt", "r_alt"], 0],
+    ["64%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow"], 0],
+    ["68%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2"], 0],
+    ["75%", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function"], 0],
+    ["Tenkeyless", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function"], 0],
+    ["1800", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function", "numpad"], 0],
+    ["Fullsized", ["esc", "topnum", "alpha", "caps", "l_ctrl", "r_ctrl", "l_os", "r_os", "l_alt", "r_alt", "arrow", "function2", "function", "numpad"], 0]
   ];
-  for (var i in formfactors) {
-    i[1].filter(features => desired.indexOf(features) != -1);
-  }
+  let importance = {
+    "esc" : 2,
+    "alpha" : 3,
+    "caps" : 1,
+    "numpad" : 2,
+    "topnum" : 2,
+    "function" : 1,
+    "function2" : 1,
+    "l_ctrl" : 0.25,
+    "r_ctrl" : 0.25,
+    "l_os" : 0.25,
+    "r_os" : 0.25,
+    "l_alt" : 0.25,
+    "r_alt" : 0.25,
+    "arrow" : 2,
+  };
+  formfactors.forEach(i=>{
+    i[1].forEach(j=>{
+      if (desired.indexOf(j) != -1) {
+        i[2] += importance[j];
+      } else {
+        i[2] -= importance[j];
+      }
+    });
+  });
+  let k = 3; // sensitivity
+  return formfactors.sort((a, b) => b[2]-a[2]).filter(e => formfactors[0][2]-k <= e[2]);
 }
